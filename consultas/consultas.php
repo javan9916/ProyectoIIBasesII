aeeab1b448f70c957779a86862170f6607928d7a
javan9916 <jantono9916@gmail.com>
2019-10-26 19:59:14 -0600

    diseno consultas

--- consultas/consultas.php
+++ consultas/consultas.php
@@ -1,86 +1,145 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Consultas</title>
-    <link rel="stylesheet" href="consulta.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-          crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-dark bg-dark">>
-  <a class="navbar-brand" href="../index.php">
-    Volver
-  </a>
-</nav>
-<div class="card ">
-    <div class="card margin">
-        <form action="consultas1.php" method="POST">
-            <div class="card-body">
-                <h5 class="card-title">Promedio de distancia diaria recorrida por un Bus.</h5>
-                <div class="form-group col-mb-16">
-                    <a class="card-subtitle mb-2 text-muted">Fecha inicio: </a> 
-                    <input type="date" id="start" name="trip-start"
-                        value="2019-10-26"
-                        min="1999-01-01" max="2020-12-31">
-                    <a class="card-subtitle mb-2 text-muted">Fecha final: </a> 
-                    <input type="date" id="start" name="trip-start"
-                        value="2019-10-26"
-                        min="1999-01-01" max="2020-12-31">
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <input name="usuario" class="form-control" id="usuario" placeholder="Nombre de Usuario">
+    <div class="container">
+    <br>
+    <br>
+        <div class="row">
+            <div class="card" style="margin-right: 20px;">
+                <div class="card-body" style="width: 520px; text-align: center;">
+                    <h5 class="card-title">Detalle de rutas de vehículos por tipo de servicio de transporte</h5>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <input type="date" id="start_cons1" name="start_cons1"
+                                    value="2019-10-25"
+                                    min="2010-01-01" max="2030-12-31">
+                            <input type="date" id="end_cons1" name="end_cons1"
+                                    value="2019-10-25"
+                                    min="2010-01-01" max="2030-12-31">
+                            <select class="select-type">
+                                <option value="bus">Bus</option>
+                                <option value="taxi">Taxi</option>
+                                <option value="tren">Tren</option>
+                            </select>
+                        </div>
+                        <table class="table">
+                            <thead class="thead-light">
+                                <tr>
+                                <th scope="col">Ruta</th>
+                                <th scope="col">Tipo de vehículo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr scope="row">
+                                    <td>12452351</td>
+                                    <td>Bus</td>
+                                </tr>
+                                <tr scope="row">
+                                    <td>12565431</td>
+                                    <td>Taxi</td>
+                                </tr>
+                                <tr scope="row">
+                                    <td>12456541</td>
+                                    <td>Tren</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn btn-primary">Consultar</button>
                     </div>
-
-                    <div class="form-group col-md-6">
-                        <input name="ruta" class="form-control" id="ruta" placeholder="Nombre de Ruta">
-                    </div>
-                </div>
-                <div class="col-mb-12">
-                    <button href="#" type="submit" class="btn btn-primary consultar" onclick="iniciar_indicaciones()" >Consultar</button>
                 </div>
             </div>
-        </form>
-    </div>
-
-    <div class="card margin">
-        <form action="consultas1.php" method="POST">
-            <div class="card-body">
-                <h5 class="card-title">Usuarios con mayor número de rastreos.</h5>
-                <div class="form-group col-mb-16">
-                    <a class="card-subtitle mb-2 text-muted">Fecha inicio: </a> 
-                    <input type="date" id="start" name="trip-start"
-                        value="2019-10-26"
-                        min="1999-01-01" max="2020-12-31">
-                    <a class="card-subtitle mb-2 text-muted">Fecha final: </a> 
-                    <input type="date" id="start" name="trip-start"
-                        value="2019-10-26"
-                        min="1999-01-01" max="2020-12-31">
-                </div>
-                <div class="col-mb-12">
-                    <button href="#" type="submit" class="btn btn-primary consultar" onclick="iniciar_indicaciones()" >Consultar</button>
+            <div class="card" >
+            <br>
+                <div class="card-body" style="width: 520px; text-align: center;">
+                    <h5 class="card-title">Promedio de distancia diaria recorrida por bus</h5>
+                    <div class="col-md">
+                        <table class="table">
+                            <thead class="thead-light">
+                                <tr>
+                                <th scope="col">Bus</th>
+                                <th scope="col">Promedio distancia diaria recorrida</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr scope="row">
+                                    <td>Limón-Shangai</td>
+                                    <td>10km</td>
+                                </tr>
+                                <tr scope="row">
+                                    <td>CQ-Parrita</td>
+                                    <td>14km</td>
+                                </tr>
+                                <tr scope="row">
+                                    <td>Puerto Viejo-Disneylandia</td>
+                                    <td>16km</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn btn-primary">Consultar</button>
+                    </div>
                 </div>
             </div>
-        </form>
+        </div>
+        <div class="row">
+            <div class="card" style="top: 20px; margin-right: 20px;">
+                <div class="card-body" style="width: 520px; text-align: center;">
+                    <h5 class="card-title">Tiempos totales anuales de rastreo para un vehículo determinado</h5>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Nombre del vehiculo">
+                        </div>
+                        <button type="button" class="btn btn-primary">Consultar</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="card" style="top: 20px;">
+                <div class="card-body" style="width: 520px; text-align: center;">
+                    <h5 class="card-title">Usuarios con mayor número de rastreos</h5>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <input type="date" id="start_cons2" name="start_cons2"
+                                    value="2019-10-25"
+                                    min="2010-01-01" max="2030-12-31">
+                            <input type="date" id="end_cons2" name="end_cons2"
+                                    value="2019-10-25"
+                                    min="2010-01-01" max="2030-12-31">
+                        </div>
+                        <table class="table">
+                            <thead class="thead-light">
+                                <tr>
+                                <th scope="col">Usuario</th>
+                                <th scope="col">Número de rastreos</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr scope="row">
+                                    <td>Mark</td>
+                                    <td>10</td>
+                                </tr>
+                                <tr scope="row">
+                                    <td>Lucy</td>
+                                    <td>9</td>
+                                </tr>
+                                <tr scope="row">
+                                    <td>John</td>
+                                    <td>5</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn btn-primary">Consultar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
-
-<!-- <a class="card-subtitle mb-2 text-muted">Tipo: </a>  
-    <button class="btn btn-secondary dropdown-toggle" type="button" 
-            id="dropdownMenuButton" data-toggle="dropdown" 
-            aria-haspopup="true" aria-expanded="false">
-        Vehiculo
-    </button>
-    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item">Bus</a>
-        <a class="dropdown-item">Taxi</a>
-        <a class="dropdown-item">Tren</a>
-    </div>  -->
